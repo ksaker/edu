d479b87634dc5f4d3137d08dd0271ea4223890d3
platonov <ksaker@yandex.ru>
2017-05-18 21:16:14 +0300

    Updated QuickSort with iterative Updated Stream api

--- src/main/java/ru/platonov/edu/sort/QuickSort.java
+++ src/main/java/ru/platonov/edu/sort/QuickSort.java
@@ -1,6 +1,7 @@
 package ru.platonov.edu.sort;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Created by platonov on 14/02/17.
@@ -31,13 +32,13 @@ public class QuickSort {
             }
 
             while (mas[rightIndex] > pivot) {
-                rightIndex --;
+                rightIndex--;
             }
 
-            if (leftIndex <= rightIndex){
+            if (leftIndex <= rightIndex) {
                 swap(leftIndex, rightIndex);
-                leftIndex ++;
-                rightIndex --;
+                leftIndex++;
+                rightIndex--;
             }
         }
 
@@ -48,12 +49,59 @@ public class QuickSort {
         if(rightIndex > startMarginIndex) {
             sort(startMarginIndex, rightIndex);
         }
+
     }
 
     private void swap(int leftIndex, int rightIndex) {
         int tmp = mas[leftIndex];
         mas[leftIndex] = mas[rightIndex];
         mas[rightIndex] = tmp;
+    }
+
+    public void iterativeSort() {
+        iterative(0, mas.length - 1);
+    }
+
+    private void iterative(int startMarginIndex, int leftMarginIndex) {
+        Stack<Range> rangeStack = new Stack<>();
+        rangeStack.push(new Range(startMarginIndex, leftMarginIndex));
+
+        while(!rangeStack.isEmpty()) {
+            Range range = rangeStack.pop();
+            int leftIndex = range.getLeftMarginIndex();
+            int rightIndex = range.getRightMarginIndex();
+
+            if (leftIndex <= rightIndex) {
+                continue;
+            }
+
+            int pivot = mas[leftIndex + (rightIndex - leftIndex) / 2];
+
+            while (leftIndex <= rightIndex) {
+                while (mas[leftIndex] < pivot) {
+                    leftIndex++;
+                }
+
+                while (mas[rightIndex] > pivot) {
+                    rightIndex--;
+                }
+
+                if (leftIndex <= rightIndex) {
+                    swap(leftIndex, rightIndex);
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+
+            if(leftIndex < range.getRightMarginIndex()) {
+               rangeStack.push(new Range(leftIndex, range.getRightMarginIndex()));
+            }
+
+            if(rightIndex > range.getLeftMarginIndex()) {
+               rangeStack.push(new Range(range.getLeftMarginIndex(), rightIndex));
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -64,6 +112,29 @@ public class QuickSort {
         new QuickSort(mas).sort();
 
         System.out.println(Arrays.toString(mas));
+
+        int[] mas2 = {1, 3, 2, 9, 10, 3, 2, 1};
+
+        new QuickSort(mas).iterativeSort();
+
+        System.out.println(Arrays.toString(mas));
     }
 
+    private static class Range {
+        private final int leftMarginIndex;
+        private final int rightMarginIndex;
+
+        private Range(int leftMarginIndex, int rightMarginIndex) {
+            this.leftMarginIndex = leftMarginIndex;
+            this.rightMarginIndex = rightMarginIndex;
+        }
+
+        public int getLeftMarginIndex() {
+            return leftMarginIndex;
+        }
+
+        public int getRightMarginIndex() {
+            return rightMarginIndex;
+        }
+    }
 }
--- src/main/java/ru/platonov/edu/sort/StreamApi.java
+++ src/main/java/ru/platonov/edu/sort/StreamApi.java
@@ -3,11 +3,18 @@ package ru.platonov.edu.sort;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * Created by platonov on 23/02/17.
@@ -26,7 +33,7 @@ public class StreamApi {
         IntStream chars = "abc".chars();
         Stream<Integer> builder = Stream.<Integer>builder().add(1).add(2).add(3).build();
         Stream<Integer> iterate = Stream.iterate(1, a -> a + 1);//Создание бесконечного стрима начиная от 1 с приращением на 1
-        Stream<String> generate = Stream.generate(() -> "a1");//Генерирует стрим из одного элемента a1
+        Stream<String> generate = Stream.generate(() -> "a1");//Генерирует бесконечный стрим из элемента a1
 
         //9
     }
@@ -46,13 +53,14 @@ public class StreamApi {
 //                .mapToInt()
 //                .mapToDouble()
 //                .mapToLong()
+//                .mapToObj()
 //                .flatMap((Function<Integer, Stream<?>>) integer -> integer) //Посмотреть какой пример дают
 //                .flatMapToInt()
 //                .flatMapToDouble()
 //                .flatMapToLong()
                 .forEach(System.out::print); //Терминальная функция нужна только для вывода результата!
 
-        //14
+        //15
     }
 
     public void terminal() {
@@ -60,7 +68,7 @@ public class StreamApi {
 
         elementIntegerStream.findFirst().get();
         elementIntegerStream.findAny().get();
-        elementIntegerStream.collect(Collectors.toList());
+        elementIntegerStream.collect(toList());
 
         elementIntegerStream.count();
         elementIntegerStream.anyMatch(a -> a == 2);
@@ -70,6 +78,7 @@ public class StreamApi {
         elementIntegerStream.max(Integer::compareTo).get();
         elementIntegerStream.min(Integer::compareTo).get();
         IntStream.empty().average();
+        IntStream.empty().sum();
 
         elementIntegerStream.forEach(System.out::print);
         elementIntegerStream.forEachOrdered(System.out::print);
@@ -77,7 +86,7 @@ public class StreamApi {
         elementIntegerStream.toArray();
         elementIntegerStream.reduce((a1, a2) -> a1 + a2);
 
-        //14
+        //15
     }
 
     public void shortCircuit(){
@@ -98,6 +107,7 @@ public class StreamApi {
         elementIntegerStream.count();
 
         elementIntegerStream.average();
+        elementIntegerStream.sum();
         Stream<Integer> stream = elementIntegerStream.mapToObj(Integer::new); //Возвращает обратно в объектный стрим
 
         //4
@@ -113,8 +123,58 @@ public class StreamApi {
         //3
     }
 
-    public static void main(String[] args) {
+    public void collectors() {
+        toList();
+        toCollection(ArrayList::new); //Возвращает коллекцию, фабрику которой передали
+        toSet();
+        toMap(
+                value -> "key" + value, //формирование ключа карты
+                value -> value,          //формирование значения карты
+                (value1, value2) -> value1 + ", " + value2 //Функция, которая в случае колизии обработает оба ЗНАЧЕНИЯ
+                                                           //для одного и того же ключа.
+        );
+        toConcurrentMap(                //обладает лучшей производительностью при параллельном стриме
+                value -> "key" + value, //формирование ключа карты
+                value -> value,          //формирование значения карты
+                (value1, value2) -> value1 + ", " + value2 //Функция, которая в случае колизии обработает оба ЗНАЧЕНИЯ
+                                                            //для одного и того же ключа.
+        );
+        averagingInt(p -> (int) p); //Получает среднеарифметическое от стрима, можно прирастить элемент
+        averagingDouble(p -> (double) p);
+        averagingLong(p -> (long) p);
+        summarizingInt(p -> (int) p); //Просуммирует все члены стрима с приращением элемента
+        summarizingDouble(p -> (double) p);
+        summarizingLong(p -> (long) p);
+        partitioningBy(p -> (int) p > 2); //Разделить стрим по какому либо принцыпу на Map<Boolean, ElementType>: true: {1,2,3,4}; false {5, 6, 7, 8}
+//        groupingBy();
+//        mapping();
 
+    }
+
+    public static void myCollector() {
+        //String concatenation collector;
+        Stream<String> stringStream = Stream.of("String1", "String2", "String3", "String4");
+        System.out.println(stringStream.parallel().collect(
+                Collector.of(
+                        StringBuilder::new, // метод инициализации аккумулятора
+                        (acc, element) -> acc.append(element).append(", "), //добавление очередного элемента
+                        (acc1, acc2) -> acc1.append(acc2), //Метод соединения двух аккумуляторов
+                        StringBuilder::toString //Метод последней обработки аккумуляторов
+                )
+        ));
+    }
+
+    public static void partitioningByExample() {
+        Stream<String> stringStream = Stream.of("String1", "String2", "String3", "String4", "String4", "String3");
+        Map<Boolean, List<String>> collect = stringStream.collect(partitioningBy(s ->  new Integer(s.substring(6)) > 3));
+    }
+
+    public static void main(String[] args) {
+        Stream<String> stringStream = Stream.of("String1", "String2", "String3", "String4", "String4", "String3");
+        Map<Boolean, List<String>> collect = stringStream.collect(partitioningBy(s ->  new Integer(s.substring(6)) > 3));
+
+
+        System.out.println("test");
     }
 
 }
